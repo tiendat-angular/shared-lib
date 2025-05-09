24e373c8c9d505e487b92823d411f23964e95593
ntdat104 <tiendat.simplize@gmail.com>
2025-05-09 17:09:14 +0700

    update

--- projects/ui/src/public-api.ts
+++ projects/ui/src/public-api.ts
@@ -2,9 +2,9 @@
  * Public API Surface of ui
  */
 
-export * from './lib/ui.service';
-export * from './lib/ui.component';
+// export * from './lib/ui.service';
+// export * from './lib/ui.component';
 
 // src/shared/projects/ui/src/public-api.ts
 export * from './lib/ui.module';  // Ensure this is exporting UiModule
-export * from './lib/button/button.component';  // Optional: if you want to export specific components directly
+// export * from './lib/button/button.component';  // Optional: if you want to export specific components directly
